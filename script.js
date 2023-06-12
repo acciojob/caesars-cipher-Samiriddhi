3bc8c220fab7432257faee2daf8380d90f1df28d
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-06-13 00:07:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,19 +32,21 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr =""; 
-	  for(let k in lookup){
-		  for(let i=0;i< encodedStr.length();i++){
-			  if(k==encodedStr[i]){
-				  decodedArr+=lookup.k;
-			  }else{
-				  decodedArr+=encodedStr[i];
-			  }
-		  }
-	  }
-	  return decodedArr;
-   }
-
+  const words = encodedStr.split(" "); // String to Array
+  let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    let decoded_word = "";
+    for (let j = 0; j < word.length; j++) {
+      const char = word.charAt(j);
+      const decoded_char = lookup[char];
+      decoded_word += decoded_char;
+    }
+    decodedArr.push(decoded_word);
+  }
+  return decodedArr.join(" ");
+}
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(rot13("SERR YBIR? NPPVBWBO"));
